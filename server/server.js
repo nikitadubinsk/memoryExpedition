c123aac684e19980d0f1e1209d5246467386cc5c
Nikita Dubinsky <nikitadubinsk@gmail.com>
2021-01-14 14:55:07 +0300

    FIX: исправление

--- server/server.js
+++ server/server.js
@@ -8,13 +8,28 @@ const path = require("path");
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// Парсинг json - application/json
+app.use(bodyParser.json());
 
-// parse application/json
-app.use(bodyParser.json())
+// Парсинг запросов по типу: application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+  );
+  next();
+});
 
 // Работа со статическими файлами
 app.use(express.static(path.join(__dirname, "../dist")));
