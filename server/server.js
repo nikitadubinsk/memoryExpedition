f58251387aac94f32ccd7fee9a09bc9426b96ae9
Nikita Dubinsky <nikitadubinsk@gmail.com>
2021-01-14 14:48:11 +0300

    FIX: исправление сервера

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "../dist")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../dist/memoryExpedition/index.html"));
 });
 
 
